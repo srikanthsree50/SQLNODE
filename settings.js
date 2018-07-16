bca1c22c51e72eb44cbbe4d535e33bc17446df71
srikanth <srikanthsree50@gmail.com>
2018-07-16 21:24:16 +0530

    final commit

---
+++ settings.js
@@ -0,0 +1,12 @@
+﻿exports.dbConfig = {
+    server:"SRINIVAS-2504\\SQLEXPRESS",
+    database:"EmployeeDB",
+    user:"sa",
+    password:"9603313561",
+    port:"1433"
+};
+
+exports.webPort = 8080;
+
+exports.httpMsgsFormat = "HTML";
+
